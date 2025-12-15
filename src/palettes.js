eafab46121164e782e1f8f8cd11e9d7659c2a0c0
Steve Cross <sd_cross@yahoo.com>
2025-12-14 23:50:13 -0800

    refactor: copy color palettes into palettes module (no behavior change)

---
+++ src/palettes.js
@@ -0,0 +1,33 @@
+export const PALETTES = [
+  {
+    name: "Natural",
+    colors: {
+      water:  0x3aa0ff,
+      sand:   0xe8d39a,
+      grass:  0x6fcf70,
+      forest: 0x2f8a4e,
+      rock:   0x9a9ea6
+    }
+  },
+  {
+    name: "Dream",
+    colors: {
+      water:  0x7ea6ff,
+      sand:   0xffd8a8,
+      grass:  0xa3ffbe,
+      forest: 0x74d68c,
+      rock:   0xb5b9ff
+    }
+  },
+  {
+    name: "Shadow",
+    colors: {
+      water:  0x4b7bbd,
+      sand:   0xbda977,
+      grass:  0x4f8f59,
+      forest: 0x2f5f42,
+      rock:   0x6f7683
+    }
+  }
+];
+
